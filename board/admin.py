3a7f8cb240d4fd40f456eeb5ecb42bdca41add8d
RootToBranch <stdodentysoung@gmail.com>
2024-05-28 15:17:45 +0900

    Create model 'TempUser'

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models as m
+
+@admin.register(m.TempUser)
+class TempUserAdmin(admin.ModelAdmin):
+    ...
--- board/models.py
+++ board/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class TempUser(models.Model):
+    username = models.CharField(max_length=32)
+    age = models.PositiveSmallIntegerField(default=0)
