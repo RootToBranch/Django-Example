222e61742eee5a326ea72f034e60142c0eae5561
RootToBranch <stdodentysoung@gmail.com>
2024-06-04 10:52:11 +0900

    Delete Model 'TempUser'

--- board/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from . import models as m
-
-@admin.register(m.TempUser)
-class TempUserAdmin(admin.ModelAdmin):
-    ...
--- board/models.py
+++ board/models.py
@@ -1,5 +1,1 @@
 from django.db import models
-
-class TempUser(models.Model):
-    username = models.CharField(max_length=32)
-    age = models.PositiveSmallIntegerField(default=0)
